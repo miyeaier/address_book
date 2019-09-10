23dffcafb4d8c772cc2b50a993fb19b77b2f23f2
miyesier <miyesier@gmail.com>
2019-09-10 13:51:52 +0200

    first green test

--- features/step_definitions/basic-steps.js
+++ features/step_definitions/basic-steps.js
@@ -1,4 +1,4 @@
-const { Given, Then } = require("cucumber");
+const { Given, Then,When } = require("cucumber");
 
 Given("I visit the site", function() {
   // Write code here that turns the phrase above into concrete actions
@@ -9,3 +9,24 @@ Then("I should see {string}", function(string) {
   // Write code here that turns the phrase above into concrete actions
   return "pending";
 });
+
+When('I click {string}', async function(string) {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+Then('I fill in {string} with {string}', async function(string, string2) {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+Then('I should have {int} contact in my address book', async function(int) {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+Then('I should not see {string}', async function(string) {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
