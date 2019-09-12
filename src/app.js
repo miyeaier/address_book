0b0d6ab95585052afa5acb199de40818abcb6e02
AlexSaxena <saxenaalex@gmail.com>
2019-09-12 11:23:41 +0200

    adds basic address profiles

--- src/app.js
+++ src/app.js
@@ -14,18 +14,18 @@ const renderContacts = () => {
       contacts.forEach(contact => {
         let li = document.createElement('li')
         li.innerHTML = `
-          <div class="card">
-            <div class="image">
-              <img src="https://ca-address-book.herokuapp.com/images/pine.jpg" />
-            </div>
-            <div class="content">
-              <h1>${ contact.name }</h1>
-              <h2>${ contact.company }</h2>
-              <p>${ contact.notes }</p> 
-              ${ contact.email } | 
-              <a href="https://www.twitter.com/${ contact.twitter}">@${contact.twitter}</a>
+        <div class="ui cards">
+        <div class="card">
+          <div class="content">
+            <div class="header">${ contact.name }</div>
+            <div class="meta">${ contact.company }</div>
+            <a href="https://www.twitter.com/${ contact.twitter}">@${contact.twitter}</a>
+            <div class="notes">${ contact.email }</div>
+            <div class="description">
+            ${ contact.notes }
             </div>
           </div>
+        </div>
        `
         ul.appendChild(li)
       })
@@ -74,3 +74,112 @@ document.addEventListener('DOMContentLoaded', () => {
        renderContacts()
     })
   })
+
+/*
+<div class="ui cards">
+  <div class="card">
+    <div class="content">
+      <div class="header">${ contact.name }</div>
+      <div class="meta">${ contact.company }</div>
+      <a href="https://www.twitter.com/${ contact.twitter}">@${contact.twitter}</a>
+      <div class="notes">${ contact.email }</div>
+      <div class="description">
+      ${ contact.notes }
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="content">
+      <div class="header">Veronika Ossi</div>
+      <div class="meta">Friend</div>
+      <div class="description">
+        Veronika Ossi is a set designer living in New York who enjoys kittens, music, and partying.
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="content">
+      <div class="header">Jenny Hess</div>
+      <div class="meta">Friend</div>
+      <div class="description">
+        Jenny is a student studying Media Management at the New School
+      </div>
+    </div>
+  </div>
+</div>
+*/
+
+
+/*
+  <div class="ui link cards">
+  <div class="card">
+    <div class="image">
+      <img src="/images/avatar2/large/matthew.png">
+    </div>
+    <div class="content">
+      <div class="header">`${ contact.name }`</div>
+      <div class="meta">
+        <a>Friends</a>
+      </div>
+      <div class="description">
+        Matthew is an interior designer living in New York.
+      </div>
+    </div>
+    <div class="extra content">
+      <span class="right floated">
+        Joined in 2013
+      </span>
+      <span>
+        <i class="user icon"></i>
+        75 Friends
+      </span>
+    </div>
+  </div>
+  <div class="card">
+    <div class="image">
+      <img src="/images/avatar2/large/molly.png">
+    </div>
+    <div class="content">
+      <div class="header">Molly</div>
+      <div class="meta">
+        <span class="date">Coworker</span>
+      </div>
+      <div class="description">
+        Molly is a personal assistant living in Paris.
+      </div>
+    </div>
+    <div class="extra content">
+      <span class="right floated">
+        Joined in 2011
+      </span>
+      <span>
+        <i class="user icon"></i>
+        35 Friends
+      </span>
+    </div>
+  </div>
+  <div class="card">
+    <div class="image">
+      <img src="/images/avatar2/large/elyse.png">
+    </div>
+    <div class="content">
+      <div class="header">Elyse</div>
+      <div class="meta">
+        <a>Coworker</a>
+      </div>
+      <div class="description">
+        Elyse is a copywriter working in New York.
+      </div>
+    </div>
+    <div class="extra content">
+      <span class="right floated">
+        Joined in 2014
+      </span>
+      <span>
+        <i class="user icon"></i>
+        151 Friends
+      </span>
+    </div>
+  </div>
+</div>
+*/
